f4cb6c47fd341b44fa6a05619a9f74e566197d29
SyntaxAdi <aadityapawar8209@gmail.com>
2026-05-15 19:42:00 +0530

    Remove Firebase dependency

--- services/bookingService.js
+++ services/bookingService.js
@@ -1,17 +1,8 @@
-const crypto = require('crypto');
 const env = require('../config/env');
+const Booking = require('../models/Booking');
 const { bookings } = require('../data/mockData');
 const CustomError = require('../utils/customError');
 const { getSpecialistById } = require('./specialistService');
-
-let Booking;
-let db;
-
-if (env.dbProvider === 'mongodb') {
-  Booking = require('../models/Booking');
-} else {
-  db = require('../config/firebase');
-}
 
 const createBooking = async ({ userName, specialistId, bookingDate, bookingTime }) => {
   const specialist = await getSpecialistById(specialistId);
@@ -31,14 +22,8 @@ const createBooking = async ({ userName, specialistId, bookingDate, bookingTime 
     createdAt: new Date().toISOString(),
   };
 
-  if (env.dbProvider === 'mongodb') {
-    const booking = await Booking.create(bookingData);
-    return { id: booking._id.toString(), ...bookingData };
-  } else {
-    const booking = { id: crypto.randomUUID(), ...bookingData };
-    bookings.push(booking);
-    return booking;
-  }
+  const booking = await Booking.create(bookingData);
+  return { id: booking._id.toString(), ...bookingData };
 };
 
 module.exports = {
--- services/specialistService.js
+++ services/specialistService.js
@@ -1,59 +1,32 @@
-const env = require("../config/env");
-const Specialist = require("../models/Specialist");
-const db = require("../config/firebase");
+const Specialist = require('../models/Specialist');
 
 const getSpecialists = async ({ category }) => {
-  if (env.dbProvider === 'mongodb') {
-    const query = category ? { specialization: category } : {};
-    return await Specialist.find(query).lean();
-  }
-
-  let query = db.collection("specialists");
-  if (category) {
-    query = query.where("specialization", "==", category);
-  }
-  const snapshot = await query.get();
-  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  const query = category ? { specialization: category } : {};
+  const specialists = await Specialist.find(query).lean();
+  return specialists.map(s => ({
+    id: s._id.toString(),
+    ...s
+  }));
 };
 
 const createSpecialist = async (data) => {
-  if (env.dbProvider === 'mongodb') {
-    const doc = await Specialist.create(data);
-    return { id: doc._id.toString(), ...doc.toObject() };
-  }
-
-  const docRef = await db.collection("specialists").add(data);
-  return { id: docRef.id, ...data };
+  const doc = await Specialist.create(data);
+  return { id: doc._id.toString(), ...doc.toObject() };
 };
 
 const updateSpecialist = async (id, data) => {
-  if (env.dbProvider === 'mongodb') {
-    const doc = await Specialist.findByIdAndUpdate(id, data, { new: true });
-    return doc ? { id: doc._id.toString(), ...doc.toObject() } : null;
-  }
-
-  await db.collection("specialists").doc(id).update(data);
-  return { id, ...data };
+  const doc = await Specialist.findByIdAndUpdate(id, data, { new: true });
+  return doc ? { id: doc._id.toString(), ...doc.toObject() } : null;
 };
 
 const deleteSpecialist = async (id) => {
-  if (env.dbProvider === 'mongodb') {
-    await Specialist.findByIdAndDelete(id);
-    return { id };
-  }
-
-  await db.collection("specialists").doc(id).delete();
+  await Specialist.findByIdAndDelete(id);
   return { id };
 };
 
 const getSpecialistById = async (id) => {
-  if (env.dbProvider === 'mongodb') {
-    const doc = await Specialist.findById(id).lean();
-    return doc ? { id: doc._id.toString(), ...doc } : null;
-  }
-
-  const doc = await db.collection("specialists").doc(id).get();
-  return doc.exists ? { id: doc.id, ...doc.data() } : null;
+  const doc = await Specialist.findById(id).lean();
+  return doc ? { id: doc._id.toString(), ...doc } : null;
 };
 
 module.exports = {
