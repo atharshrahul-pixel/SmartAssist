d208bc1cd8957617553baeaa59a6b3a1f621f19a
SyntaxAdi <aadityapawar8209@gmail.com>
2026-06-02 21:48:29 +0530

    feat(triage): refine fallback response messaging for ideal category recommendations

--- services/triageService.js
+++ services/triageService.js
@@ -142,7 +142,7 @@ const getFallbackResponse = (category, userMsgs) => {
     let text = `Based on your answers, we recommend a ${category} for your symptoms.`;
     
     if (idealCategory !== category) {
-      text = `We don't have a ${idealCategory} right now, but we suggest you visit a ${category} first.`;
+      text = `We don't have a ${idealCategory} right now.`;
     } else {
       if (category === 'Physiotherapist') {
         text = 'Based on your answers, we recommend a Physiotherapist to assess and guide you.';
